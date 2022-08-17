7a067a818328291494e804879db539a4e15211c0
Duncan Gatuthu <dgatuthu@gmail.com>
2022-08-17 12:30:47 +0300

    Fetch data from files

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const http = require("http");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
   res.end("Response from server")
